2cc6570b7f8553fdea6e6585e60e126a1ec63c0a
aprilia15 <apriliapermata28@gmail.com>
2019-12-09 17:12:22 +0700

    [Aprilia] Blog

--- routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const usersController = require('./controllers/usersController.js');
-const authenticate = require('./middlewares/authenticate.js');
-
-router.post('/auth/register', usersController.register);
-router.post('/auth/login', usersController.login);
-router.get('/auth/whoAmI', authenticate, usersController.whoAmI);
-
-module.exports = router;
