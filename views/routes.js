d211710a59b076c32760f25e024c885b3e68997c
aprilia15 <apriliapermata28@gmail.com>
2019-12-09 17:18:01 +0700

    [Aprilia] Blog

--- views/routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authenticate = require('./middlewares/authenticate.js');
-
-router.get('/', authenticate, function(req, res) {
-  res.render('index');
-})
-
-module.exports = router;
